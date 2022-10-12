08fd3423c0a743f0402d83e58173cd8f781c79ff
Ladislav Reismüller <ladislavreismuller@gmail.com>
2022-10-12 18:01:37 +0200

    odcestinovani_v1.py

---
+++ odcestinovani_v1.py
@@ -0,0 +1,16 @@
+def odcesti(text):
+    word = ''
+
+    for lett in text:
+        cz_letters = 'ěščřžýáíéúůťďň'
+        en_letters = 'escrzyaieuutdn'
+
+        if lett.isupper(): cz_letters, en_letters = cz_letters.upper(), en_letters.upper()
+
+        if lett in cz_letters:
+            word += en_letters[cz_letters.index(lett)]
+            continue
+        word += lett
+    return word
+
+print(odcesti(input('text: ')))
