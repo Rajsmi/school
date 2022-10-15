d209b02c159379c95a0ed5757daa608ad31634e7
Ladislav Reismüller <ladislavreismuller@gmail.com>
2022-10-15 11:50:33 +0200

    odcestinovani_v1.py

--- odcestinovani_v1.py
+++ odcestinovani_v1.py
@@ -5,12 +5,14 @@ def odcesti(text: str) -> str:
         cz_letters = 'ěščřžýáíéúůťďň'
         en_letters = 'escrzyaieuutdn'
 
-        if lett.isupper(): cz_letters, en_letters = cz_letters.upper(), en_letters.upper()
+        if lett.isupper():
+            cz_letters, en_letters = cz_letters.upper(), en_letters.upper()
 
         if lett in cz_letters:
             word += en_letters[cz_letters.index(lett)]
             continue
         word += lett
     return word
+
 
 print(odcesti(input('text: ')))
